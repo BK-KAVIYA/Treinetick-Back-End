4c5a7eccdb2491537d664cecda7a62619a1712d5
Baladurage Kavinda <63805835+BK-KAVIYA@users.noreply.github.com>
2024-06-24 12:50:27 +0530

    add milestone service and controller

--- src/main/java/com/treinetick/repository/MilestoneRepository.java
+++ src/main/java/com/treinetick/repository/MilestoneRepository.java
@@ -1,6 +1,5 @@
 package com.treinetick.repository;
 
-import com.treinetick.model.ActivityLog;
 import com.treinetick.model.Milestone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
