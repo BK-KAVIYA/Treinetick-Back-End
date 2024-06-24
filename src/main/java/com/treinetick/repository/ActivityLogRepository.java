b47cfbb106abcc066d31412f2b9089c639490e78
Baladurage Kavinda <63805835+BK-KAVIYA@users.noreply.github.com>
2024-06-24 11:36:59 +0530

    activityLog service controller create

--- src/main/java/com/treinetick/repository/ActivityLogRepository.java
+++ src/main/java/com/treinetick/repository/ActivityLogRepository.java
@@ -1,13 +1,12 @@
 package com.treinetick.repository;
 
 import com.treinetick.model.ActivityLog;
-import com.treinetick.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface ActivityLogRepository extends JpaRepository<ActivityLog, String> {
-//    Optional<User> findByProjectID(String projectID);
+
 }
 
